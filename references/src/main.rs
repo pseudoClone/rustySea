eef3a7d80a1a1d19e9e9a9bbab9f56177d39e599
pseudoclone <prince.077bei027@acem.edu.np>
2023-10-05 06:51:24 +0545

    new: references in rust guide

---
+++ references/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let s1 = String::from("Hello");
+    let len = calculate_size(&s1);
+    println!("The length of {} is {}.", s1, len);
+
+
+
+    let mut s2 = String::from("Hello");
+    change(&mut s2);
+}
+
+fn calculate_size(s: &String) -> usize {
+    s.len()
+}
+
+fn change(s2: &mut String) {
+    s2.push_str(", World");  // Only mutable references can be mutated
+}
+
+// cannot have two or mutable references to the same object
+// cannot have a mutable reference while already immutable reference exists
